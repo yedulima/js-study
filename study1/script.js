2766114b2112a1a0e039a8a7cf2bfdffd09d4d01
Eduardo Lima <108648404+yedulima@users.noreply.github.com>
2023-11-06 10:11:04 -0300

    FEAT / Adicionado arquivos

---
+++ study1/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let button = document.querySelector("button");
+    button.addEventListener("click", function(){
+        alert("Button pressed!");
+    })
+})
